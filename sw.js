f990f9ecdaf4cb16109313196c46104600fe8d93
rubsrueda <rueda.castillo@gmail.com>
2026-01-28 03:41:08 +0000

    Fix: Force cache bust v1738080000 - Update SW to clear caches

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
+// Service Worker v1738080000 - Forzar actualización
 self.addEventListener('install', (e) => {
+    console.log('SW instalando v1738080000');
     self.skipWaiting();
 });
 
+self.addEventListener('activate', (e) => {
+    console.log('SW activando v1738080000');
+    // Limpiar todos los caches
+    e.waitUntil(
+        caches.keys().then(names => {
+            return Promise.all(
+                names.map(name => caches.delete(name))
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
 self.addEventListener('fetch', (e) => {
-    // Solo para permitir la instalación
-    e.respondWith(fetch(e.request));
+    // NO cachear - siempre fetch desde red
+    e.respondWith(
+        fetch(e.request, { cache: 'no-store' })
+            .catch(() => fetch(e.request))
+    );
 });
