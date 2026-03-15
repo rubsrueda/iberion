eb3be58680429b9c64332a41252e8c4780a552a0
rubsrueda <rueda.castillo@gmail.com>
2026-03-15 02:15:57 -0600

    Update tutorialScripts.js

--- tutorialScripts.js
+++ tutorialScripts.js
@@ -1,6 +1,4 @@
 // En tutorialScripts.js
-import { INTRO_STEPS } from './introTutorial.js';
-import { INVASION_STEPS } from './invasionSteps.js';
 
 console.log("tutorialScripts.js CARGADO - v12 (Estructura Modular)");
 
