6af742135cc3fbdef0a790fea1370e0868644fda
rubsrueda <rueda.castillo@gmail.com>
2026-03-15 02:19:19 -0600

    Update introTutorial.js

--- introTutorial.js
+++ introTutorial.js
@@ -1,6 +1,6 @@
 // introTutorial.js
 
-export const INTRO_STEPS = [
+const INTRO_STEPS = [
     { id: 'TUT_01', message: "¡Saludos, General! El Reino te ha confiado la conquista de este archipiélago. Toca para continuar.", actionCondition: () => true },
     { id: 'TUT_02', message: "Tu objetivo es capturar la Capital enemiga o lograr la victoria por puntos. Toca para continuar.", onStepStart: () => centerMapOn(2, 6), actionCondition: () => true },
     { id: 'TUT_03', message: "Empecemos por preparar tu panel de mando. Toca para continuar.", onStepStart: () => centerMapOn(3, 1), actionCondition: () => true },
