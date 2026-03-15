2cf1cd40400447747fc6018185902e421fb641a4
rubsrueda <rueda.castillo@gmail.com>
2026-03-15 04:15:01 -0600

    Update tutorialScripts.js

--- tutorialScripts.js
+++ tutorialScripts.js
@@ -5,6 +5,7 @@ console.log("tutorialScripts.js CARGADO - v12 (Estructura Modular)");
 const TUTORIAL_SCRIPTS = {
     archipelagoInvasor: [
         ...INTRO_STEPS,    // Viene de introTutorial.js
+        ...INTROMAPA,     // Viene de intromapa.js
         ...INVASION_STEPS  // Viene de invasionSteps.js
     ]
 };
